442adc66559e0535a74befaa0bfc0cecee1a5306
flintKing12 <jonathanaurugai@outlook.com>
2018-07-22 10:04:38 +0300

    make it better

--- api/app_test.py
+++ api/app_test.py
@@ -1,25 +1,28 @@
 import unittest
 import json
-from .app import app
+from . import app
 
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.test_client()
+        self.app = app.app.test_client()
         self.entry_id = 1
 
     def test_entry_list(self):
+        """tests for all entries in the data storage"""
         resp = self.app.get('/api/v1/entries')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.content_type, 'application/json')
 
     def test_single_entry(self):
+        """ tests for a single entry """
         resp = self.app.get('/api/v1/entries/{}'.format(self.entry_id))
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.content_type, 'application/json')
         content = json.loads(resp.get_data())
-        self.assertEqual(content, {'entry': dict(description='lorem ipsum', entry_id=1, title='jonathan in never land')})
+        self.assertEqual(content, dict(entry=dict(description='lorem ipsum', entry_id=1,
+                                                  title='jonathan in never land')))
 
 
 if __name__ == '__main__':
