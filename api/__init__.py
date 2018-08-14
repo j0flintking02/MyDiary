2f641be9b3527a0ca77119003de686df5497dd69
flintKing12 <jonathanaurugai@outlook.com>
2018-08-14 09:10:04 +0300

    fix confilicts

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flasgger import Swagger
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
 Swagger = Swagger(app)
+CORS(app)
+
 
 from api import entries
 
